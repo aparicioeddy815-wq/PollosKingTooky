b546550749b07fa3e723fa8b218e804bbaf176a6
eddy <aparicioeddy815@gmail.com>
2026-09-19 01:31:14 -0400

    Creacion de tablas y entidades

---
+++ backend/src/main/java/com/kingtooky/backend/domain/Usuario.java
@@ -0,0 +1,49 @@
+package com.kingtooky.backend.domain;
+
+public class Usuario {
+    private Integer idUsuario;
+    private Integer idRol;
+    private String nombreUsuario;
+    private String contrasena;
+    private String estado;
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Integer getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(Integer idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
